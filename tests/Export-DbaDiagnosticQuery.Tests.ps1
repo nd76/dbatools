1d7972a9e8ff0df869dc1bae8e0673299b72bc61
André Kamman <andrekamman@gmail.com>
2017-10-08 15:26:34 +0200

    Added tests for DbaDiagnostic etc
    testing newly built switches in result set and also if exporting functions correctly.
    Fixed a typo in constants.ps1

--- tests/Export-DbaDiagnosticQuery.Tests.ps1
+++ tests/Export-DbaDiagnosticQuery.Tests.ps1
@@ -3,7 +3,7 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
 Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
-	AfterAll {
+	AfterEach {
 		(Get-ChildItem "$env:temp\dbatoolsci") | Remove-Item
 	}
 	Context "Verifying output" {
@@ -11,5 +11,33 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
 			$results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance2 -QueryName 'Memory Clerk Usage' | Export-DbaDiagnosticQuery -Path "$env:temp\dbatoolsci"
 			(Get-ChildItem "$env:temp\dbatoolsci").Count | Should Be 1
 		}
+		It "exports results to Excel file" {
+			$results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance2 -QueryName 'Memory Clerk Usage' | Export-DbaDiagnosticQuery -Path "$env:temp\dbatoolsci" -ConvertTo Excel
+			(Get-ChildItem "$env:temp\dbatoolsci\*.xlsx").Count | Should Be 1
+		}
+		It "exports results to .sql files" {
+			$results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance2 -QueryName 'Top Logical Reads Queries' | Export-DbaDiagnosticQuery -Path "$env:temp\dbatoolsci" -ConvertTo Excel
+			(Get-ChildItem "$env:temp\dbatoolsci\*.sql").Count | Should BeGreaterThan 0
+		}
+		It "exports results to .plan files" {
+			$results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance2 -QueryName 'Top Logical Reads Queries' | Export-DbaDiagnosticQuery -Path "$env:temp\dbatoolsci" -ConvertTo Excel
+			(Get-ChildItem "$env:temp\dbatoolsci\*.sqlplan").Count | Should BeGreaterThan 0
+		}
+        It "does not export results to .sql files if -NoQueryExport switch is uses" {
+			$results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance2 -QueryName 'Top Logical Reads Queries' | Export-DbaDiagnosticQuery -Path "$env:temp\dbatoolsci" -ConvertTo Excel -NoQueryExport
+			(Get-ChildItem "$env:temp\dbatoolsci\*.sql").Count | Should Be 0
+        }
+        It "still exports results to .plan files if -NoQueryExport switch is used" {
+			$results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance2 -QueryName 'Top Logical Reads Queries' | Export-DbaDiagnosticQuery -Path "$env:temp\dbatoolsci" -ConvertTo Excel -NoQueryExport
+			(Get-ChildItem "$env:temp\dbatoolsci\*.sqlplan").Count | Should BeGreaterThan 0
+        }
+        It "does not export results to .plan files if -NoPlanExport switch is used" {
+			$results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance2 -QueryName 'Top Logical Reads Queries' | Export-DbaDiagnosticQuery -Path "$env:temp\dbatoolsci" -ConvertTo Excel -NoPlanExport
+			(Get-ChildItem "$env:temp\dbatoolsci\*.sqlplan").Count | Should Be 0
+        }
+        It "still exports results to .sql files if -NoPlanExport switch is used" {
+			$results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance2 -QueryName 'Top Logical Reads Queries' | Export-DbaDiagnosticQuery -Path "$env:temp\dbatoolsci" -ConvertTo Excel -NoPlanExport
+			(Get-ChildItem "$env:temp\dbatoolsci\*.sql").Count | Should BeGreaterThan 0
+        }
 	}
 }
--- tests/Invoke-DbaDiagnosticQuery.Tests.ps1
+++ tests/Invoke-DbaDiagnosticQuery.Tests.ps1
@@ -12,5 +12,29 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
 			$results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance1 -DatabaseSpecific *>&1
 			$results.Name.Count -gt 10 | Should Be $true
 		}
+        It "Uncomments Complete Query Text columns" {
+            $results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance1 -QueryName "Top Logical Reads Queries" *>&1
+            ($results.Result | Get-Member | Where-Object Name -eq "Complete Query Text").Count | Should Be 1
+        }
+        It "Uncomments Query Plan columns" {
+            $results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance1 -QueryName "Top Logical Reads Queries" *>&1
+            ($results.Result | Get-Member | Where-Object Name -eq "Query Plan").Count | Should Be 1
+        }
+        It "Does not uncomment Complete Query Text columns when -NoQueryTextColumn is used" {
+            $results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance1 -QueryName "Top Logical Reads Queries" -NoQueryTextColumn *>&1
+            ($results.Result | Get-Member | Where-Object Name -eq "Complete Query Text").Count | Should Be 0
+        }
+        It "Does not uncomment Query Plan columns when -NoPlanColumn is used" {
+            $results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance1 -QueryName "Top Logical Reads Queries" -NoPlanColumn *>&1
+            ($results.Result | Get-Member | Where-Object Name -eq "Query Plan").Count | Should Be 0
+        }
+        It "Still uncomments Complete Query Text columns when -NoPlanColumn is used" {
+            $results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance1 -QueryName "Top Logical Reads Queries" -NoPlanColumn *>&1
+            ($results.Result | Get-Member | Where-Object Name -eq "Complete Query Text").Count | Should Be 1
+        }
+        It "Still Uncomments Query Plan columns when -NoQueryTextColumn is used" {
+            $results = Invoke-DbaDiagnosticQuery -SqlInstance $script:instance1 -QueryName "Top Logical Reads Queries" -NoQueryTextColumn *>&1
+            ($results.Result | Get-Member | Where-Object Name -eq "Query Plan").Count | Should Be 1
+        }
 	}
 }
